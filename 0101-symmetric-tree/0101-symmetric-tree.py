ad4186a2686391143cbcc116f2a887131949a8e9
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-06-08 02:50:39 +0530

    Time: 48 ms (50.07%), Space: 16.4 MB (58.9%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,17 +4,39 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+#         def isMirror(node1, node2):
+#             if not node1 and not node2:
+#                 return True
+#             if not node1 or not node2:
+#                 return False
+#             return (
+#                 node1.val == node2.val
+#                 and isMirror(node1.left, node2.right)
+#                 and isMirror(node1.right, node2.left)
+#             )
+        
+#         return isMirror(root, root)
+from collections import deque
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def isMirror(node1, node2):
-            if not node1 and not node2:
-                return True
-            if not node1 or not node2:
-                return False
-            return (
-                node1.val == node2.val
-                and isMirror(node1.left, node2.right)
-                and isMirror(node1.right, node2.left)
-            )
+        if not root:
+            return True
         
-        return isMirror(root, root)
+        queue = deque([(root.left, root.right)])
+        
+        while queue:
+            node1, node2 = queue.popleft()
+            
+            if not node1 and not node2:
+                continue
+            
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            
+            queue.append((node1.left, node2.right))
+            queue.append((node1.right, node2.left))
+        
+        return True
